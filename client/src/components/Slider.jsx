6888467face9037d7190415c8601f37e8a6c0c98
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-19 15:49:57 +0000

    Created image slider component

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,30 +1,107 @@
-import { ArrowLeftOutlined } from "@mui/icons-material";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
+import { useState } from "react";
 import styled from "styled-components";
+import dummy from "../assets/dummy1.png";
 
 const Container = styled.div`
   width: 100%;
-  min-height: 100vh;
+  height: 100vh;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
   width: 3rem;
-  min-height: 3rem;
+  height: 3rem;
   background-color: whitesmoke;
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "0.6rem"};
+  right: ${(props) => props.direction === "right" && "0.6rem"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(0vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.bg};
+`;
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 80%;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 3rem;
+`;
+
+const Title = styled.h1`
+  font-size: 4.3rem;
+`;
+
+const Description = styled.p`
+  margin: 3rem 0rem;
+  font-size: 1.25rem;
+  font-weight: 600;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  padding: 0.625rem;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {};
+
   return (
     <Container>
-      <Arrow />
-      <ArrowLeftOutlined />
-      <Arrow />
-      <Arrow />
-      <ArrowRightOutlined />
-      <Arrow />
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <Slide bg="#f5fafd">
+          <ImgContainer>
+            <Image src={dummy} />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Description>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF NEW ARRIVALS
+            </Description>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
   );
 };
