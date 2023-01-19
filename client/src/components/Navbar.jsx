72ea43367300a46ce2d9ff149f546f11953ffdeb
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-19 14:06:26 +0000

    Re-usable navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import Badge from "@mui/material/Badge";
+
+const Container = styled.div`
+  min-height: 4rem;
+`;
+
+const Wrapper = styled.div`
+  padding: 0.6rem 1.2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 0.85rem;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 1.5rem;
+  padding: 0.3rem;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  font-size: 2rem;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 0.85;
+  cursor: pointer;
+  margin-left: 1.5rem;
+`;
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search..." />
+            <Search style={{ color: "gray", fontSize: "1rem" }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>SHIV.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
