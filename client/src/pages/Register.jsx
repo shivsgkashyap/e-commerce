973fc6f7b080ef503fb74fb3642848c498249f9a
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-19 22:22:36 +0000

    Register page component

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 1.25rem;
+  background-color: white;
+  border-radius: 1rem;
+  ${mobile({ width: "75%" })}
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 1.25rem 0.625rem 0rem 0rem;
+  padding: 0.625rem;
+`;
+
+const Agreement = styled.span`
+  font-size: 0.75rem;
+  margin: 1.25rem 0rem;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 1rem 1.25rem;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="First Name" />
+          <Input placeholder="Last Name" />
+          <Input placeholder="Email" />
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+          <Input placeholder="Confirm Password" />
+          <Agreement>
+            By creating a new account, I consent to the processing of my
+            personal information in accordance with the <b>Privacy Policy</b>
+          </Agreement>
+          <Button>CREATE ACCOUNT</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
