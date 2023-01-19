e836f92f6f0e73850fa5a48a7ef42381b317431b
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-19 17:49:59 +0000

    Created fake newsletter component

---
+++ client/src/components/Newsletter.jsx
@@ -0,0 +1,61 @@
+import { Send } from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60vh;
+  background-color: #fcf5f5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 4.375rem;
+  margin-bottom: 1.25rem;
+`;
+
+const Description = styled.p`
+  font-size: 1.5rem;
+  font-weight: 200;
+  margin-bottom: 1.25rem;
+`;
+
+const InputContainer = styled.div`
+  width: 50%;
+  min-height: 2.5rem;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
+
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding: 1.25rem;
+`;
+
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  background-color: teal;
+  color: white;
+`;
+
+const Newsletter = () => {
+  return (
+    <Container>
+      <Title>Newsletter</Title>
+      <Description>Get timely updates from your favorite products.</Description>
+      <InputContainer>
+        <Input placeholder="Your email" />
+        <Button>
+          <Send />
+        </Button>
+      </InputContainer>
+    </Container>
+  );
+};
+
+export default Newsletter;
