eb87555a36c02e7168ff3fe63e2220a760766d12
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-19 16:32:42 +0000

    Categories component

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { categories } from "../data";
+import CategoryItem from "./CategoryItem";
+
+const Container = styled.div`
+  display: flex;
+  padding: 1.25rem;
+  justify-content: space-between;
+`;
+
+const Categories = () => {
+  return (
+    <Container>
+      {categories.map((item) => (
+        <CategoryItem item={item} />
+      ))}
+    </Container>
+  );
+};
+
+export default Categories;
