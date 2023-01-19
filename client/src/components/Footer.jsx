1ca92e75f724d65f54ebabc41dd88b01ffbe09b2
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-19 20:05:03 +0000

    Completed footer with social media icons

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@mui/icons-material";
+import {
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -14,24 +22,59 @@ const Left = styled.div`
 
 const Logo = styled.h1``;
 
-const Description = styled.p``;
+const Description = styled.p`
+  margin: 1.25rem 0rem;
+`;
 
-const SocialContainer = styled.div``;
+const SocialContainer = styled.div`
+  display: flex;
+`;
 
-const SocialIcon = styled.div``;
+const SocialIcon = styled.div`
+  width: 2.5rem;
+  min-height: 2.5rem;
+  border-radius: 50%;
+  color: white;
+  background-color: ${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 1.25rem;
+`;
 
 const Center = styled.div`
   flex: 1;
   padding: 1.25rem;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 1.875rem;
+`;
+
+const List = styled.ul`
+  list-style: none;
+  display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 0.625rem;
 `;
 
 const Right = styled.div`
   flex: 1;
   padding: 1.25rem;
-  flex-wrap: wrap;
-  justify-content: space-between;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 1.25rem;
+  display: flex;
+  align-items: bottom;
+`;
+
+const Payment = styled.img`
+  width: 50%;
 `;
 
 const Footer = () => {
@@ -46,22 +89,50 @@ const Footer = () => {
           autem voluptates assumenda.
         </Description>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="#3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="#E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="#55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="#E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Mens Fashion</ListItem>
+          <ListItem>Womens Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "0.625rem" }} />
+          622 Dixie Path, South Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "0.625rem" }} />
+          +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "0.625rem" }} />
+          contact@cutandsew.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"></Payment>
+      </Right>
     </Container>
   );
 };
