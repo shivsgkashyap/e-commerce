ab94339b33a3d7a103430c0733595c1d32284ea3
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-19 22:23:57 +0000

    Cart page component

---
+++ client/src/pages/Cart.jsx
@@ -0,0 +1,225 @@
+import { Add, Remove } from "@mui/icons-material";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 1.25rem;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const TopButton = styled.button`
+  padding: 0.625rem;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => (props.type === "filled" ? "none" : "1px solid black")};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``;
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0rem 0.625rem;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+`;
+
+const Image = styled.img`
+  width: 12.5rem;
+`;
+
+const Details = styled.div`
+  padding: 1.25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 1.25rem;
+  height: 1.25rem;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.25rem;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 1.5rem;
+  margin: 0.3rem;
+`;
+
+const ProductPrice = styled.div`
+  font-size: 1.875rem;
+  font-weight: 200;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 0.625rem;
+  padding: 1.25rem;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 1.875rem 0rem;
+  display: flex;
+  justify-content: space-between
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "1.5rem"};
+`;
+
+const SummaryItemText = styled.span`
+  flex: 1;
+`;
+const SummaryItemPrice = styled.span`
+  flex: 1;
+`;
+
+const SummaryButton = styled.button`
+  width: 100%;
+  padding: 0.625rem;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist(4)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail></ProductDetail>
+              <Image></Image>
+              <Details>
+                <ProductName>
+                  <b>Product:</b>
+                </ProductName>
+                <ProductId>
+                  <b>ID:</b>
+                </ProductId>
+                <ProductColor color="black" />
+                <ProductSize>
+                  <b>Size:</b>
+                </ProductSize>
+              </Details>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice></ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
