5d28a7b1082fb3b45c352c821d86c5de8b95862c
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-20 00:07:53 +0000

    Imported all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Cart />
     </div>
   );
 }
