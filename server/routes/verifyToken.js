f621aa0ee8b8cd3922b067eef6bcaf7576f1b222
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-20 13:58:53 +0000

    Verify and authorize jwt token

---
+++ server/routes/verifyToken.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.SEC_KEY, (err, user) => {
+      if (err) {
+        res.status(403).json("Invalid token!");
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("Access Denied");
+  }
+};
+
+const verifyTokenAndAuth = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Access Denied");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuth };
