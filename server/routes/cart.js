f9edd1f15607957283bbd1d13355b297f5207dae
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-20 17:47:03 +0000

    CRUD functionality for cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,12 +1,68 @@
+const Cart = require("../models/Cart");
+const {
+  verifyToken,
+  verifyTokenAndAuth,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("hello world!");
+// CREATE
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
-  const username = req.body.username;
-  console.log(username);
+// UPDATE
+router.put("/:id", verifyTokenAndAuth, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
+router.delete("/:id", verifyTokenAndAuth, async function (req, res) {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart successfully deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET USER CART
+router.get("/find/:userId", verifyTokenAndAuth, async function (req, res) {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET ALL
+router.get("/", verifyTokenAndAdmin, async function (req, res) {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(carts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
