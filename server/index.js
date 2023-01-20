b573fe2be2a23210e1e100f4b39f0068c8f26780
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-20 17:03:43 +0000

    Added product route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json());
 /* ROUTES */
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3000;
