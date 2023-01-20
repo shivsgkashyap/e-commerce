fdef3c628b29d34f3190328e4fd8d3e75f705537
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-20 13:58:12 +0000

    Added jwt authorization

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -37,9 +37,18 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Incorrect password!");
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.SEC_KEY,
+      { expiresIn: "7d" }
+    );
+
     const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
