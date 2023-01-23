a40a5d65379503dca9c501067c1a7d8e2d6b2e94
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-23 15:31:35 +0000

    Added error handling

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -24,8 +24,10 @@ const Products = ({ cat, filters, sort }) => {
             ? `http://localhost:3000/api/products?category=${cat}`
             : "http://localhost:3000/api/products"
         );
-        setFilteredProducts(res.data);
-      } catch (err) {}
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
     };
     getProducts();
   }, [cat]);
@@ -41,11 +43,29 @@ const Products = ({ cat, filters, sort }) => {
       );
   }, [products, cat, filters]);
 
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
-      {filteredProducts.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {cat
+        ? filteredProducts.map((item) => <Product item={item} key={item.id} />)
+        : products
+            .slice(0, 8)
+            .map((item) => <Product item={item} key={item.id} />)}
     </Container>
   );
 };
