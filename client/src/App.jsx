4b871035088faa4f1c0a64a9fac0733393224c3a
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-23 21:19:09 +0000

    Success path +  redirection for logged in user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+import { useSelector } from "react-redux";
 
 function App() {
   return (
@@ -13,11 +20,15 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductList />}>
           <Route path=":category" element={<ProductList />} />
-          <Route path=":id" element={<Product />} />
         </Route>
+        <Route path="/product/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
       </Routes>
     </Router>
   );
