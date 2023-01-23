33ae5f630a47b26489f4a8cbf9395bb59794215f
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-23 14:13:42 +0000

    Added stripe payment route

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 /* ROUTES */
 app.use("/api/auth", authRoute);
@@ -18,6 +21,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 3000;
