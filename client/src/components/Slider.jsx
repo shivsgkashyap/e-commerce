1c1960dc3e9cf528662938d9a3ecfab6f7ac0682
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-25 12:19:48 +0000

    Added working links to slider component

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,5 +1,6 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { sliderItems } from "../data";
 import { mobile } from "../responsive";
@@ -83,12 +84,18 @@ const Button = styled.button`
 
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
+  const navigate = useNavigate();
+
   const handleClick = (direction) => {
     if (direction === "left") {
       setSlideIndex((slideIndex) => (slideIndex > 0 ? slideIndex - 1 : 2));
     } else {
       setSlideIndex((slideIndex) => (slideIndex < 2 ? slideIndex + 1 : 0));
     }
+  };
+
+  const handleButtonClick = (cat) => {
+    navigate(`products/${cat}`);
   };
 
   return (
@@ -105,7 +112,9 @@ const Slider = () => {
             <InfoContainer>
               <Title>{item.title}</Title>
               <Description>{item.desc}</Description>
-              <Button>SHOP NOW</Button>
+              <Button onClick={() => handleButtonClick(item.cat)}>
+                SHOP NOW
+              </Button>
             </InfoContainer>
           </Slide>
         ))}
