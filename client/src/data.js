a87091c5b39b2c2f6314aa037a937460f224efbd
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-25 12:18:46 +0000

    Added categories to slider component

--- client/src/data.js
+++ client/src/data.js
@@ -9,6 +9,7 @@ export const sliderItems = [
     title: "SUMMER SALE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "#f5fafd",
+    cat: "summer",
   },
   {
     id: 2,
@@ -16,6 +17,7 @@ export const sliderItems = [
     title: "AUTUMN COLLECTION",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "#fcf1ed",
+    cat: "autumn",
   },
   {
     id: 3,
@@ -23,6 +25,7 @@ export const sliderItems = [
     title: "LOUNGEWEAR LOVE",
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: "#fbf0f4",
+    cat: "loungewear",
   },
 ];
 
@@ -31,19 +34,19 @@ export const categories = [
     id: 1,
     img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "SHIRT STYLE!",
-    cat: "women",
+    cat: "shirts",
   },
   {
     id: 2,
     img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     title: "LOUNGEWEAR LOVE",
-    cat: "coat",
+    cat: "loungewear",
   },
   {
     id: 3,
     img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     title: "LIGHT JACKETS",
-    cat: "jeans",
+    cat: "jackets",
   },
 ];
 
