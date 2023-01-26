4f47c3e14db8d14ea2aa3656ff9c6edc6228b4e5
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-26 12:35:37 +0000

    Added react strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,11 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
