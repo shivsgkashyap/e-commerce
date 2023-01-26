88ffabb685440f5b44faa6f7d2c2f82983a0f9a3
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-26 12:41:02 +0000

    Improved backend user model schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,9 +2,35 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 2,
+      max: 50,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     isAdmin: { type: Boolean, default: false },
     img: { type: String },
   },
