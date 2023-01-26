de20fef1e0817d0b0ce4db457bdd30f7e62084c1
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-26 12:40:20 +0000

    Add, remove and clear cart from redux

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -9,12 +9,27 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
+      const item = state.products.find((item) => item.id === action.payload.id);
+      if (item) {
+        item.quantity += action.payload.quantity;
+      } else {
+        state.products.push(action.payload);
+      }
       state.quantity += 1;
-      state.products.push(action.payload);
       state.total += action.payload.price * action.payload.quantity;
+    },
+    removeProduct: (state, action) => {
+      state.products = state.products.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+    clearProducts: (state) => {
+      state.products = [];
+      state.quantity = 0;
+      state.total = 0;
     },
   },
 });
 
-export const { addProduct } = cartSlice.actions;
+export const { addProduct, removeProduct, clearProducts } = cartSlice.actions;
 export default cartSlice.reducer;
