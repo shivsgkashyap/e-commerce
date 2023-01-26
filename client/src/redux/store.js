ef6437f82a8aab95a2c4efb09bfa35b57a4f8f84
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-26 12:40:32 +0000

    Combined redux reducers in store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -19,7 +19,10 @@ const persistConfig = {
   storage,
 };
 
-const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
