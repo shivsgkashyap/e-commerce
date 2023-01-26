39c074c75fbd4bda86317ce6d8f5ee937c2f59a4
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-26 18:22:05 +0000

    Added featured product component to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
-import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const Home = () => {
   return (
@@ -15,7 +15,7 @@ const Home = () => {
       <main>
         <Slider />
         <Categories />
-        <Products />
+        <FeaturedProducts />
         <Newsletter />
       </main>
       <Footer />
